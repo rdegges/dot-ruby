688c1ce68610f48e804b24bb817d4e6ac53ecfe2
Randall Degges <rdegges@home.lan>
2020-04-10 15:06:29 -0700

    Using printf instead of echo (test).

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,12 @@ mkdir -p "$(rbenv root)"/plugins
 
 # Install `ruby-build` so we can install arbitrary Ruby releases
 # REF: https://github.com/rbenv/ruby-build
-echo "\nInstalling ruby-build..."
+printf "\nInstalling ruby-build..."
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 # Install the latest core Ruby release
-echo "\nInstalling latest stable release of Ruby..."
+printf "\nInstalling latest stable release of Ruby..."
 release="$(rbenv install --list | grep "^[0-9]" | grep -v "[a-z]" | tail -n 1)"
 rbenv install $release
 
-echo "Done!"
+printf "Done!"
