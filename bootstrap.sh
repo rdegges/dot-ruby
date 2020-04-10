ce450011dd4223d681b632398cf68d008e7df845
Randall Degges <rdegges@home.lan>
2020-04-10 15:07:28 -0700

    Using printf+removing newlines.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,17 @@
 
 # Install `rbenv` to manage all Ruby versions
 # REF: https://github.com/rbenv/rbenv
-echo "Installing rbenv..."
+printf "Installing rbenv..."
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 mkdir -p "$(rbenv root)"/plugins
 
 # Install `ruby-build` so we can install arbitrary Ruby releases
 # REF: https://github.com/rbenv/ruby-build
-printf "\nInstalling ruby-build..."
+printf "Installing ruby-build..."
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 # Install the latest core Ruby release
-printf "\nInstalling latest stable release of Ruby..."
+printf "Installing latest stable release of Ruby..."
 release="$(rbenv install --list | grep "^[0-9]" | grep -v "[a-z]" | tail -n 1)"
 rbenv install $release
 
