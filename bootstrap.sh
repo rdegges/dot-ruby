73dc0c9aabf56e2942cf94680b061b0083a3446a
Randall Degges <rdegges@home.lan>
2020-04-10 15:04:11 -0700

    Adding bootstrap script for Ruby.
    This installs rbenv, ruby-build, and the latest stable Ruby release.

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Install `rbenv` to manage all Ruby versions
+# REF: https://github.com/rbenv/rbenv
+echo "Installing rbenv..."
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+mkdir -p "$(rbenv root)"/plugins
+
+# Install `ruby-build` so we can install arbitrary Ruby releases
+# REF: https://github.com/rbenv/ruby-build
+echo "\nInstalling ruby-build..."
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+# Install the latest core Ruby release
+echo "\nInstalling latest stable release of Ruby..."
+release="$(rbenv install --list | grep "^[0-9]" | grep -v "[a-z]" | tail -n 1)"
+rbenv install $release
+
+echo "Done!"
